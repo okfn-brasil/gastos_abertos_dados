5e24f9523f1eb8c041ea681db50b66c95e5f6c85
AndresMRM <andres@inventati.org>
2016-09-18 06:56:18 -0300

    more one case to normalize...

--- utils/execucao_downloader.py
+++ utils/execucao_downloader.py
@@ -86,7 +86,11 @@ def normalize_csv(csv_path):
                     value = datetime.datetime.strptime(value,
                                                        '%Y-%m-%d %H:%M:%S')
                 except ValueError:
-                    value = datetime.datetime.strptime(value, '%Y-%m-%d')
+                    try:
+                        value = datetime.datetime.strptime(value, '%Y-%m-%d')
+                    except ValueError:
+                        value = datetime.datetime.strptime(
+                            value.split()[0], '%Y-%m-%d')
         return datetime.datetime.strftime(value, '%Y-%m-%d')
     for colname in ['datainicial', 'datafinal']:
         if colname in table.columns:
